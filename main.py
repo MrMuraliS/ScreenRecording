a97cb669c812711aced373675d2aab3c0cb63045
muralis <muralis1223@gmail.com>
2022-09-03 15:49:06 +0530

    Adding changes

--- main.py
+++ main.py
@@ -1,45 +1,29 @@
-import cv2
+from PIL import ImageGrab
 import numpy as np
+import cv2
 import pyautogui
-from PIL import Image
+import datetime
+
 
 screen_size = pyautogui.size()
-print(screen_size)
-print(screen_size.width)
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter("output.mp4", fourcc, 20.0, (screen_size.width, screen_size.height))
-print(out)
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+time_stamp = datetime.datetime.now().strftime("%d-%m-%Y %H-%M-%S")
+out = cv2.VideoWriter(f"{time_stamp}.mp4", fourcc, 20, (screen_size.width, screen_size.height))
 
-webcam = cv2.VideoCapture(0)
-
+# webcam = cv2.VideoCapture(0)
 
 while True:
-    # Capture the screen
-    img = pyautogui.screenshot()
+    img = ImageGrab.grab(bbox=(0, 0, screen_size.width, screen_size.height))
+    img_np = np.array(img)
+    img_final = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+    # _, frame = webcam.read()
+    # fr_height, fr_width, _ = frame.shape
+    # img_final[0:fr_height, 0:fr_width, :] = frame[0:fr_height, 0:fr_width, :]
+    cv2.imshow("Capture", img_final)
+    # cv2.imshow('Webcam', frame)
+    out.write(img_final)
 
-    # Convert the image into numpy array
-    img = np.array(img)
-
-    # Convert the color space from BGR to RGB
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    _, frame = webcam.read()
-    # Finding the width, height and shape of our webcam image
-    fr_height, fr_width, _ = frame.shape
-    # setting the width and height properties
-    img[0:fr_height, 0: fr_width, :] = frame[0:fr_height, 0: fr_width, :]
-
-    #cv2.imshow('frame', img)
-
-    # Write the frame into the file 'output.avi'
-    out.write(img)
-
-    # Press 'q' to quit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        print("Recording Stopped")
+    if cv2.waitKey(10) == ord('q'):
         break
-
-out.release()
-cv2.destroyAllWindows()
 
